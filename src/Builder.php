5791f997dbaa07a354ad6b1896ca8fd38a51c29e
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2018-10-25 14:12:22 +0300

    add comments

--- src/Builder.php
+++ src/Builder.php
@@ -13,12 +13,12 @@ class Builder
     private $query;
 
     /**
-     * @var array queries
+     * @var array $queries
      */
     private $queries = [];
 
     /**
-     * @var array indices
+     * @var array $indices
      */
     private $indices = [];
 
@@ -36,15 +36,12 @@ class Builder
     }
 
     /**
-     * Add new query
+     * Append a new query
+     *
+     * @param \Elastin\Builders\QueryBuilder $query
+     *
+     * @return \Elastin\Builder
      */
-    public function query(?QueryBuilder $query = null): Builder
-    {
-        $this->appendQuery($query);
-
-        return $this;
-    }
-
     public function appendQuery(?QueryBuilder $query = null): Builder
     {
         if ($query === null) {
@@ -56,6 +53,27 @@ class Builder
         return $this;
     }
 
+    /**
+     * appendQuery alias
+     *
+     * @param \Elastin\Builders\QueryBuilder $query
+     *
+     * @return \Elastin\Builder
+     */
+    public function query(?QueryBuilder $query = null): Builder
+    {
+        $this->appendQuery($query);
+
+        return $this;
+    }
+
+    /**
+     * Add index
+     *
+     * @param string $index
+     *
+     * @return \Elastin\Builder
+     */
     public function index(string $index): Builder
     {
         $this->indices[] = $index;
@@ -63,6 +81,13 @@ class Builder
         return $this;
     }
 
+    /**
+     * Add multiple indexes
+     *
+     * @param array $indices
+     *
+     * @return \Elastin\Builder
+     */
     public function indices(array $indices): Builder
     {
         $this->indices = array_merge($this->indices, $indices);
@@ -70,48 +95,84 @@ class Builder
         return $this;
     }
 
-    public function __call(string $name, array $arguments): Builder
+    /**
+     * Trap any method call that is not defined in the Builder
+     * and call it at last query object
+     *
+     * @param string $method
+     * @param array $arguments
+     *
+     * @return \Elastin\Builder
+     */
+    public function __call(string $method, array $arguments): Builder
     {
         if (count($this->queries) !== 0) {
             $queryBuilder = $this->queries[count($this->queries) - 1];
 
-            $callback = [$queryBuilder, $name];
+            $callback = [$queryBuilder, $method];
             if (is_callable($callback)) {
-                call_user_func_array([$queryBuilder, $name], $arguments);
+                call_user_func_array([$queryBuilder, $method], $arguments);
             }
         }
 
         return $this;
     }
 
-    public function _buildIndices()
+    /**
+     * Add all indices to query
+     *
+     * @return \Elastin\Builder
+     */
+    public function _buildIndices(): Builder
     {
         if (count($this->indices) > 0) {
             $indices = implode(',', $this->indices);
 
             $this->query['index'] = $indices;
         }
+
+        return $this;
     }
 
-    private function _buildQueries()
+    /**
+     * Build all queries
+     *
+     * @return \Elastin\Builder
+     */
+    private function _buildQueries(): Builder
     {
         if (count($this->queries) === 1) {
+            // Single query is an object
             $this->query['body'] = $this->queries[0]->build();
         } elseif (count($this->queries) > 1) {
+            // multiple queries is an array
             $this->query['body'] = array_map(function ($query) {
                 return $query->build();
             }, $this->queries);
         }
+
+        return $this;
     }
 
+    /**
+     * Build final query/queries
+     *
+     * @return array
+     */
     public function build(): array
     {
-        $this->_buildIndices();
-        $this->_buildQueries();
+        $this
+            ->_buildIndices()
+            ->_buildQueries();
 
         return $this->query->all();
     }
 
+    /**
+     * Build query and return it as json
+     *
+     * @return string
+     */
     public function buildJson(): ?string
     {
         $query = $this->build();
@@ -124,6 +185,11 @@ class Builder
         return $json;
     }
 
+    /**
+     * Build query and return only the body,
+     * as it would be sent in elasticsearch
+     * server.
+     */
     public function buildBodyJson(): ?string
     {
         $query = $this->build();
